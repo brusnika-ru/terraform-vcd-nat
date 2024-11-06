d06af86f3c79cf3d88cec5f093ea1ae87c3e1c00
igor Ilinykh <i.ilinykh@brusnika.ru>
2024-11-06 17:03:30 +0500

    Remove VCG

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-data "vcd_vdc_group" "orgGroup" {
-  name = var.vcd_group_name
-}
-
 data "vcd_nsxt_edgegateway" "orgEdge" {
   name     = var.vcd_edge_name
-  owner_id = data.vcd_vdc_group.orgGroup.id
 }
 
 # Создание NAT правила
